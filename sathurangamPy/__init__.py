091bcb5020c188bfb404fee6345170c5e34f0280
Sherin Nishara S <sherinars2004@gmail.com>
2024-12-14 21:56:37 -0800

    Update __init__.py

--- sathurangamPy/__init__.py
+++ sathurangamPy/__init__.py
@@ -1,22 +1,28 @@
-from sathurangamPy import display_image
-from sathurangamPy import grayscale_converter
-from sathurangamPy import resize_image
-from sathurangamPy import rotate_image
-from sathurangamPy import blur_image
-from sathurangamPy import crop_image
-from sathurangamPy import canny_edge
-from sathurangamPy import dilate_image
-from sathurangamPy import erode_image
-from sathurangamPy import contour_count
-from sathurangamPy import draw_contour
-from sathurangamPy import draw_specific_contour
-from sathurangamPy import draw_blank_image
-from sathurangamPy import draw_color_image
-from sathurangamPy import draw_rectangle
-from sathurangamPy import draw_circle
-from sathurangamPy import draw_line
-from sathurangamPy import draw_text
-from sathurangamPy import draw_custom_text
-from sathurangamPy import read_video
-from sathurangamPy import detect_face
-from sathurangamPy import save_face
+# __init__.py
+
+# Import all functions from processing_functions
+from .processing_functions import (
+    display_image,
+    grayscale_converter,
+    resize_image,
+    rotate_image,
+    blur_image,
+    crop_image,
+    canny_edge,
+    dilate_image,
+    erode_image,
+    threshold_image,
+    contour_count,
+    draw_contour,
+    draw_specific_contour,
+    draw_blank_image,
+    draw_color_image,
+    draw_rectangle,
+    draw_circle,
+    draw_line,
+    draw_text,
+    draw_custom_text,
+    read_video,
+    detect_face,
+    save_face,
+)
